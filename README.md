01d43d813f9a7742e35b49aff22270ca2cac1a21
Sarfraz Ahmed <sarfraz@onsupport.com>
2020-02-12 18:29:22 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+[![Total Downloads](https://poser.pugx.org/sarfraznawaz2005/composer-cost/downloads)](https://packagist.org/packages/sarfraznawaz2005/composer-cost)
+
+# composer-cost
+
+Simple composer plugin that displays size of each folder under `vendor` to help you with which package is taking the most disk space.
+
+## Screenshot
+
+![Main Window](https://raw.githubusercontent.com/sarfraznawaz2005/composer-cost/master/screenshot.png)
+
+## Install
+
+You can install it globally:
+
+`composer global require sarfraznawaz2005/composer-cost`
+
+or per-project bases:
+
+`composer require sarfraznawaz2005/composer-cost`
+
+That's it. Now on any project when you type `composer update` or `composer install`, it will show info like above screenshot.
+
+FYI, you can skip any plugin when installing/updating by appending `--no-plugins` argument, example:
+
+`composer update --no-plugins`
+
+Thanks
---
+++ src/ComposerCost.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace ComposerCost;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+class ComposerCost implements PluginInterface, EventSubscriberInterface
+{
+    protected $io;
+    protected $composer;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => [['postInstall']],
+            ScriptEvents::POST_UPDATE_CMD => [['postUpdate']],
+        ];
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function postInstall(Event $event)
+    {
+        $this->calculate();
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function postUpdate(Event $event)
+    {
+        $this->calculate();
+    }
+
+    /**
+     * Calculate and show package sizes
+     */
+    private function calculate()
+    {
+        $folders = [];
+        $sizes = [];
+        $vendorDir = getcwd() . DIRECTORY_SEPARATOR . 'vendor';
+
+        if (file_exists($vendorDir) && is_readable($vendorDir)) {
+            foreach (glob($vendorDir . '/*', GLOB_NOSORT) as $path) {
+                if (!is_file($path)) {
+                    $sizes[basename($path)] = $this->dirSize($path);
+                }
+            }
+        }
+
+        // sort by bigger size first
+        uasort($sizes, static function ($a, $b) {
+            return $b - $a;
+        });
+
+        foreach ($sizes as $folder => $size) {
+            $folders[] = ['Folder' => $folder, 'Size' => $this->humanSize($size)];
+        }
+
+        $this->io->write('');
+        $this->io->write('<fg=black;bg=green> Vendor Cost </>');
+        $this->table($folders);
+    }
+
+    private function dirSize($rootDir)
+    {
+        $size = 0;
+
+        foreach (glob(rtrim($rootDir, '/') . '/*', GLOB_NOSORT) as $path) {
+            $size += is_file($path) ? filesize($path) : $this->dirSize($path);
+        }
+
+        return $size;
+    }
+
+    private function humanSize($fsizebyte)
+    {
+        $size = 0;
+
+        if ($fsizebyte < 1024) {
+            $size = $fsizebyte . ' bytes';
+        } elseif (($fsizebyte >= 1024) && ($fsizebyte < 1048576)) {
+            $size = round(($fsizebyte / 1024), 2);
+            $size .= ' KB';
+        } elseif (($fsizebyte >= 1048576) && ($fsizebyte < 1073741824)) {
+            $size = round(($fsizebyte / 1048576), 2);
+            $size .= ' MB';
+        } elseif ($fsizebyte >= 1073741824) {
+            $size = round(($fsizebyte / 1073741824), 2);
+            $size .= ' GB';
+        }
+
+        return $size;
+    }
+
+    private function table($data)
+    {
+        $keys = array_keys(end($data));
+        $size = array_map('strlen', $keys);
+
+        foreach (array_map('array_values', $data) as $e) {
+            $size = array_map('max', $size,
+                array_map('strlen', $e));
+        }
+
+        foreach ($size as $n) {
+            $form[] = "%-{$n}s";
+            $line[] = str_repeat('-', $n);
+        }
+
+        $form = '| ' . implode(' | ', $form) . " |\n";
+        $line = '+-' . implode('-+-', $line) . "-+\n";
+        $rows = array(vsprintf($form, $keys));
+
+        foreach ($data as $e) {
+            $rows[] = vsprintf($form, $e);
+        }
+
+        $this->io->write('<fg=green>' . $line . implode($line, $rows) . $line . '</>');
+    }
+}
